969a06a35d9fca1f8ec87327c44e457519575292
mryurii <yurii.yakymovych@gmail.com>
2019-07-17 05:43:03 +0300

    Install shopify model

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -7,5 +7,5 @@ ShopifyApp.configure do |config|
   config.embedded_app = true
   config.after_authenticate_job = false
   config.api_version = "2019-07"
-  config.session_repository = ShopifyApp::InMemorySessionStore
+  config.session_repository = Shop
 end
